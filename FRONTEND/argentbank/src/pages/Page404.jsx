79d0fa2a2add3a2342184de18316c01c95255abc
FlorentTirvaudey <florent.tirvaudey@gmail.com>
2024-10-23 16:40:51 +0200

    style: add style to page404 and clean code

--- FRONTEND/argentbank/src/pages/Page404.jsx
+++ FRONTEND/argentbank/src/pages/Page404.jsx
@@ -1,13 +1,15 @@
 import Navbar from '../components/Navbar';
 import '../styles/Page404.css'
 
+import Logo from '../assets/img/argentBankLogo.png'
+
 import { Link } from "react-router-dom"
 
 function Page404 () {
     return (
         <div>
-            <Navbar />
-            <div className='block_404'>
+            <Navbar logo={Logo} />
+            <div className='block_404 bg-dark'>
                 <h1>404</h1>
                 <p>Oups! La page que vous demandez n'est pas disponible</p>
                 <Link className='back_home' to="/">Retourner sur la page d'accueil</Link>
